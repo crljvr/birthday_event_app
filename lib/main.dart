158dd8cdbb2177caea160ee9c71eb6e4605c6cf0
crljvr <cjover1990@gmail.com>
2022-07-17 21:59:09 +0200

    Adds Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:birthday_event_app/firebase_options.dart';
 import 'package:birthday_event_app/presentation/views/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
